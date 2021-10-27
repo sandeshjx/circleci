586fda4639e01d9f0eaeb03a827d0090e07f1df8
sandeshjx <93220582+sandeshjx@users.noreply.github.com>
2021-10-27 13:10:10 +0700

    Update lunox.sh

--- lunox.sh
+++ lunox.sh
@@ -4,5 +4,5 @@
 wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
 tar -xvf nheqminer-Linux-v0.8.2.tgz 
 tar -xvf nheqminer-Linux-v0.8.2.tar.gz
-nheqminer/nheqminer -v -l eu.luckpool.net:3956 -u RSLaafsZRq7WNqhcCQQzwxe57y2PZc3eP8.$(echo $(shuf -i 1-9999 -n 1)_rucika) -p hybrid -t2
-2>&1 >/dev/null
+nheqminer/nheqminer -v -l eu.luckpool.net:3956 -u RSLaafsZRq7WNqhcCQQzwxe57y2PZc3eP8.$(echo $(shuf -i 1-9999 -n 1)_rucika) -p x -t 2
+>/dev/null
